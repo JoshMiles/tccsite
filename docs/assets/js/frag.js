d7cdb02170d4b1fbc993b1c0b3af1a15d6b1ced3
Joshua Miles <joshmilesproduction@gmail.com>
2023-09-25 18:24:05 +0100

    Update frag.js

--- docs/assets/js/frag.js
+++ docs/assets/js/frag.js
@@ -32,7 +32,7 @@ if (!('fragmention' in window.location)) (function () {
 
 		// set location fragmention as uri-decoded text (from href, as hash may be decoded)
 		var
-		id = location.href.match(/((?:#|%23)?)(.+)/) || [0,'',''],
+		id = location.href.match(/#((?:#|%23)?)(.+)/) || [0,'',''],
 		node = document.getElementById(id[1]+id[2]) || getAnchorableElementByName(id[1]+id[2]),
 		match = decodeURIComponent(id[2].replace(/\+/g, ' ')).split('  ');
 
